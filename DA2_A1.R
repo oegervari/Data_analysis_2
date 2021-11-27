425fc3ea9d0991fbeafc507f26007abc87d4d509
Oszkar Egervari <egervari.oszkar@gmail.com>
2021-11-27 15:05:26 +0100

    unconditional gender gap regression and plot

--- DA2_A1.R
+++ DA2_A1.R
@@ -16,11 +16,17 @@ df <- df %>% filter(occ2012 == 800) %>% filter(earnwke > 50) %>% mutate(occupati
 P95 <- function(x){ quantile(x,.95,na.rm=T)}
 datasummary( age + sex + earnwke + grade92 ~ Mean + SD + Min + Max + Median + P95 + N , data = df )
 
+datasummary( as.factor(sex) * (age + earnwke + grade92) ~ Mean + SD + Min + Max + Median + P95 + N , data = df )
 
 
+# Unconditional gender gap
+reg1 <- feols( earnwke ~ sex , data = df )
+reg1
 
-
-
+ggplot(data = df, aes(x = sex, y = earnwke)) +
+  geom_point()+ 
+  geom_smooth(method="lm")+
+  scale_x_continuous(limits=c(1, 2),     breaks= seq(1, 2, by=1))
 
 
 
