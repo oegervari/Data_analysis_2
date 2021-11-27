fbc4cb36cf88091707406f120e2a80f1d26d4827
Oszkar Egervari <egervari.oszkar@gmail.com>
2021-11-27 14:56:15 +0100

    loading data and filtering

--- DA2_A1.R
+++ DA2_A1.R
@@ -7,8 +7,14 @@ library(tidyverse)
 library(ggplot2)
 library(fixest)
 
+df <- read_csv("https://raw.githubusercontent.com/oegervari/Data_analysis_2/main/data/morg-2014-emp.csv")
 
+#filtering for occupation and wages higher than 50$ / week
+df <- df %>% filter(occ2012 == 800) %>% filter(earnwke > 50) %>% mutate(occupation = 'Accountants and auditors')
 
+# Checking the data
+P95 <- function(x){ quantile(x,.95,na.rm=T)}
+datasummary( age + sex + earnwke + grade92 ~ Mean + SD + Min + Max + Median + P95 + N , data = df )
 
 
 
