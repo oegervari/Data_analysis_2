3de85124dba3def7512bd72e67631497310c23f3
Oszkar Egervari <egervari.oszkar@gmail.com>
2021-12-04 14:43:09 +0100

    adding msummary, brier score and graph

--- DA2_A2.R
+++ DA2_A2.R
@@ -65,8 +65,34 @@ print(logit_marg)
 datasummary(pred_logit + pred_probit~min+P25+Median+Mean+P75+Max,data=data[1:4200,])
 
 #Creating model summary output
-
 etable( lpm, logit, probit, digits=3 , fitstat = c('r2','pr2'))
+
+# Including the marginals:
+cm <- c('(Intercept)' = 'Constant')
+pmodels <- list('LPM'=lpm, 'logit coeffs'=logit, 'logit marginals'=logit_marg, 'probit coeffs'=probit, 'probit marginals'=probit_marg)
+
+msummary( pmodels ,
+          fmt="%.3f",
+          gof_omit = 'DF|Deviance|Log.Lik.|F|R2 Adj.|AIC|BIC|R2|PseudoR2',
+          stars=c('*' = .05, '**' = .01),
+          coef_rename = cm)
+
+#goodness of fit - brier score
+fitstat_register("brier", function(x){mean(x$residual^2)}, "Brier score")
+etable( lpm, logit, probit , drop = "factor|lspline|income|exerc",fitstat = ~ brier  )
+
+# Comparing predicted probabilities of logit and probit to LPM
+g5<-ggplot(data = data[1:4200,]) +
+  geom_point(aes(x=pred_lpm, y=pred_probit, color="Probit"), size=0.7,  shape=16) +
+  geom_point(aes(x=pred_lpm, y=pred_logit,  color="Logit"), size=0.7,  shape=16) +
+  geom_line(aes(x=pred_lpm, y=pred_lpm,    color="45 degree line"), size=0.7) +
+  labs(x = "Predicted probability of high rating (LPM)", y="Predicted probability")+
+  scale_y_continuous(expand = c(0.00,0.0), limits = c(0,1), breaks = seq(0,1,0.1)) +
+  scale_x_continuous(expand = c(0.00,0.0), limits = c(0,1), breaks = seq(0,1,0.1)) 
+
+
+
+
 
 
 
